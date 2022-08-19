56ac07c87bd1ff4667d9c9a77cf7526b340f16cb
ibnbayo <abdullahifahm@gmail.com>
2022-08-19 16:44:10 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import logo from './logo.svg';
+import { useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
+// const fetchQuotes = async () => {
+// 	const config = {
+// 		headers: {
+// 			'x-rapidapi-host': 'famous-quotes4.p.rapidapi.com',
+// 			'x-rapidapi-key': API_KEY
+// 		}
+// 	};
+// 	const res = await axios.get(
+// 		`https://famous-quotes4.p.rapidapi.com/random`,
+// 		config
+// 	);
+// 	return res.data;
+// };
+const config = {
+  headers: {
+        'X-RapidAPI-Key': 'c0d2e70417msh3bde3b7dbe9e25ap12748ejsncd1fe394742c',
+        'X-RapidAPI-Host': 'getguidelines.p.rapidapi.com'
+      }
+}
+// const options = {
+//   method: 'GET',
+//   url: 'https://getguidelines.p.rapidapi.comhttps//getguidelines.com/all',
+//   headers: {
+//     'X-RapidAPI-Key': 'c0d2e70417msh3bde3b7dbe9e25ap12748ejsncd1fe394742c',
+//     'X-RapidAPI-Host': 'getguidelines.p.rapidapi.com'
+//   }
+// };
+
 function App() {
+  const [edamamData,setEdamamData] = useState()
+
+  useEffect(()=>{
+    
+axios.get('https://getguidelines.p.rapidapi.comhttps//getguidelines.com/all', config)
+.then(res => {
+	console.log(res.data);
+})
+.catch(err => {
+	console.log(err);
+});
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        ttt
       </header>
     </div>
   );
